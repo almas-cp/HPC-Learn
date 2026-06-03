0d1e184c2611aadb8f4b5552bf31d1735ee562ad
almas <almascp11@gmail.com>
2026-06-03 16:11:33 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "./",
   plugins: [react()],
+  optimizeDeps: {
+    entries: ["index.html"]
+  },
   server: {
     port: 5000
   },
